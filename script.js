31f887df5674769427ff5cc6633691458a2d56d3
Siva Krishna <divyakola143@gmail.com>
2026-05-09 17:40:59 +0530

    Refactor showMessage function and add event listeners

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ function showMessage(){
     alert("Thank you for visiting my portfolio website!");
 
 }
+
+const buttons = document.querySelectorAll(".read-more-btn");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", function(event){
+
+        event.preventDefault();
+
+        alert("Project details page coming soon!");
+
+    });
+
+});
